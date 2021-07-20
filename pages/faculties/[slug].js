d687db1db9ab20277d0e4e1035f4fd83643c9aaa
Maxwell Kapezi Jr <maxwellkjr@gmail.com>
2021-07-20 13:35:06 -0700

    Major fixes again

--- pages/faculties/[slug].js
+++ pages/faculties/[slug].js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
@@ -39,29 +40,48 @@ export async function getStaticProps({ params }) {
 
 const Faculty = ({ faculty }) => {
   return (
-    <section className="flex flex-row justify-center items-center pt-40 pb-20 lg:pt-40 text-white h-auto bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
-      <div className="container mx-auto px-4">
-        <Link href="/faculties">
-          <a
-            className="flex flex-row justify-left items-center text-2xl md:text-4xl font-bold border-b-2 w-full md:w-1/2 pt-10 pb-4 hover:text-gray-100"
-            title="Back to faculties"
-          >
-            <FontAwesomeIcon icon={faArrowLeft} className="h-6 w-6 mr-2 mt-2" />
-            {faculty.name}
-          </a>
-        </Link>
-        {/* Programs IN THE FACULTY */}
-        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-10">
-          {faculty.programs.map((program) => (
-            <Link key={program.id} href={`/programs/${program.slug}`}>
-              <a className="flex flex-row justify-center items-center text-xl text-center font-semibold py-2 sm:px-2 sm:py-4 bg-gray-100 bg-opacity-30 rounded hover:shadow-xl">
-                {program.name}
-              </a>
-            </Link>
-          ))}
+    <>
+      <Head>
+        <title>
+          {faculty.name} | The University of Malawi Programs and Courses Guide
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="og:title"
+          content={`${faculty.name} | University of Malawi Programs and Courses Guide`}
+        />
+        <meta
+          name="og:description"
+          content="Here to guide you in choosing the right courses and route map"
+        />
+      </Head>
+      <section className="flex flex-row justify-center items-center pt-40 sm:pt-28 pb-20 lg:pt-40 text-white h-auto bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
+        <div className="container mx-auto px-4">
+          <Link href="/faculties">
+            <a
+              className="flex flex-row justify-left items-center text-2xl md:text-4xl font-bold border-b-2 w-full md:w-1/2 py-6 hover:text-gray-100"
+              title="Back to faculties"
+            >
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+                className="h-6 w-6 mr-2 mt-2"
+              />
+              {faculty.name}
+            </a>
+          </Link>
+          {/* PROGRAMS IN THE FACULTY */}
+          <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-10">
+            {faculty.programs.map((program) => (
+              <Link key={program.id} href={`/programs/${program.slug}`}>
+                <a className="flex flex-row justify-center items-center text-xl text-center font-semibold py-2 sm:px-2 sm:py-4 bg-gray-100 bg-opacity-30 rounded hover:shadow-xl">
+                  {program.name}
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- pages/programs/[slug].js
+++ pages/programs/[slug].js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
@@ -39,26 +40,45 @@ export async function getStaticProps({ params }) {
 
 const Program = ({ program }) => {
   return (
-    <section className="flex flex-row justify-center items-center text-white h-screen bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
-      <div className="container mx-auto w-full p-4">
-        <Link href={`/faculties/${program.faculty.slug}`}>
-          <a
-            className="flex flex-row justify-left items-center text-xl md:text-2xl font-bold border-b-2 w-full md:w-1/2 pb-4 from-white to-transparent hover:opacity-90"
-            title={`Back to ${program.faculty.name} programs`}
-          >
-            <FontAwesomeIcon icon={faArrowLeft} className="h-6 w-6 mr-2 mt-1" />
-            {program.name}
-          </a>
-        </Link>
-        <div className="program-details mt-4">
-          <div>
-            <p className="text-xl font-semibold">
-              Duration: {program.duration} years
-            </p>
+    <>
+      <Head>
+        <title>
+          {program.name} | The University of Malawi Programs and Courses Guide
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="og:title"
+          content={`${program.name} | University of Malawi Programs and Courses Guide`}
+        />
+        <meta
+          name="og:description"
+          content="Here to guide you in choosing the right courses and route map"
+        />
+      </Head>
+      <section className="flex flex-row justify-center items-center pt-40 sm:pt-28 text-white h-screen bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
+        <div className="container mx-auto w-full p-4">
+          <Link href={`/faculties/${program.faculty.slug}`}>
+            <a
+              className="flex flex-row justify-left items-center text-xl md:text-2xl font-bold border-b-2 w-full md:w-1/2 py-6 from-white to-transparent hover:opacity-90"
+              title={`Back to ${program.faculty.name} programs`}
+            >
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+                className="h-6 w-6 mr-2 mt-1"
+              />
+              {program.name}
+            </a>
+          </Link>
+          <div className="program-details mt-4">
+            <div>
+              <p className="text-xl font-semibold">
+                Duration: {program.duration} years
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
