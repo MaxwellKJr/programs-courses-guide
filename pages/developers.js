9442e080d2b58cc3f835145021c6b1779368e00e
Maxwell Kapezi Jr <maxwellkjr@gmail.com>
2021-07-21 09:59:34 -0700

    Fixes

--- pages/developers.js
+++ pages/developers.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 const Developers = () => {
   const developers = [
@@ -31,7 +32,7 @@ const Developers = () => {
           Group 7 Members | University of Malawi Programs and Courses Guide App
         </title>
       </Head>
-      <section className="h-auto lg:h-screen pt-40 pb-20 lg:pt-0 bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
+      <section className="h-auto lg:h-screen pt-40 pb-20 lg:pt-20 bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
         <div className="container mx-auto flex flex-col justify-center items-center h-full px-4">
           <h1 className="text-white text-2xl font-bold">Group 7 Members</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 text-center mt-10 w-full md:w-auto">
@@ -42,8 +43,12 @@ const Developers = () => {
                     key={developer.id}
                     className="flex flex-col bg-gray-100 bg-opacity-60 p-4 m-2 justify-center items-center h-60 rounded-xl"
                   >
-                    <img
+                    <Image
                       src={developer.imageUrl}
+                      placeholder="blur"
+                      blurDataURL
+                      width="112"
+                      height="112"
                       className="w-28 h-28 rounded-full"
                     />
                     <h2 className="text-xl font-semibold pt-4">
